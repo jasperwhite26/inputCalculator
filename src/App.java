6e34528274b9d12a1d8473ce73f02c0f6354e5a9
jasper <whiteana001@warren.k12.in.us>
2024-09-18 13:21:06 -0400

    initial commit

--- src/App.java
+++ src/App.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class App {
+import java.util.Scanner;
+
+public class App {
+        public static void main(String[] args) {
+            inputThenPrintSumAndAverage();
+        }
+
+        private static void inputThenPrintSumAndAverage() {
+            int sum = 0;
+            int count = 0;
+            Scanner input = new Scanner(System.in);
+
+            while(true){
+                System.out.print("Enter a number: ");
+                try {
+                    String line = input.nextLine();
+                    int num = Integer.parseInt(line);
+                    sum += num;
+                    count++;
+                } catch (Exception e){
+                    break;
+                }
+
+            }
+
+            int avg = 0;
+            if(count == 0){
+                avg = 0;
+            } else {
+                avg = sum / count;
+            }
+
+            System.out.print("SUM: " + sum + " AVG: " + avg);
+        }
+
 }
